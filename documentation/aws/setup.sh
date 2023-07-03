d04d7edd9b662ea27f750224d263bcd448828419
Benjamin Cates <bjc2dogs@gmail.com>
2023-07-03 09:56:52 -0700

    Fix miniconda sha checking

--- documentation/aws/setup.sh
+++ documentation/aws/setup.sh
@@ -11,10 +11,11 @@ sudo apt upgrade -y
 # Install miniconda
 echo "Installing miniconda"
 wget https://repo.anaconda.com/miniconda/Miniconda3-py310_23.3.1-0-Linux-x86_64.sh
-if [[ $(sha256sum Miniconda3-py310_23.3.1-0-Linux-x86_64.sh) != aef279d6baea7f67940f16aad17ebe5f6aac97487c7c03466ff01f4819e5a651 ]]; then
-  echo "File sha does not match expected sha :P  Exiting"
-  exit 1
+if ! echo "aef279d6baea7f67940f16aad17ebe5f6aac97487c7c03466ff01f4819e5a651 Miniconda3-py310_23.3.1-0-Linux-x86_64.sh" | sha256sum --check ; then
+    echo "SHA 256 checksum failed" >&2
+    exit 1
 fi
+
 bash Miniconda3-py310_23.3.1-0-Linux-x86_64.sh
 conda env create -f environment.yml
 conda activate asid
@@ -28,4 +29,4 @@ sudo apt install s3fs
 echo "Starting aws configure. Please enter your access key and secret key. Leave region and output format default.\n"
 aws configure
 
-echo "System setup done. Please run mount.sh to mount your s3 bucket."
+echo "System setup done. Please run documentation/aws/mount.sh to mount your s3 bucket."
