9918ee4ec716e7c4cec1f1b9071cd8801a14fbdb
Benjamin Cates <bjc2dogs@gmail.com>
2023-07-05 11:14:08 -0700

    Fix directory already exists error

--- classification/train.py
+++ classification/train.py
@@ -322,7 +322,8 @@ def main():
 
         if valid_map > best_valid_cmap:
             path = os.path.join("models",wandb_run.name + '.pt')
-            os.mkdir("models")
+            if not os.path.exists("models"):
+                os.mkdir("models")
             torch.save(model_for_run.state_dict(), path)
             print("Model saved in:", path)
             print(f"Validation cmAP Improved - {best_valid_cmap} ---> {valid_map}")
