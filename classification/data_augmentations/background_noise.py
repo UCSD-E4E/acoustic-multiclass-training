e98330abfa4280051f8c03ca0fe94629967a5773
Marco Bazzani <mbazzani@ucsd.edu>
2023-07-05 14:28:22 -0700

    Updated loading process

--- classification/data_augmentations/background_noise.py
+++ classification/data_augmentations/background_noise.py
@@ -24,19 +24,25 @@ class BackgroundNoise(torch.nn.Module):
         self.length = length
 
     def forward(self, clip: torch.Tensor)->torch.Tensor:
+        # If loading fails, skip for now
         try:
             noise_clip = self.choose_random_noise()
         except:
             print('Error loading noise clip, '
                   + 'background noise augmentation not performed')
             return clip
-        # If loading fails, skip for now
         return self.alpha*clip + (1-self.alpha)*noise_clip
 
     def choose_random_noise(self):
-        noise_clips = list(os.listdir(self.noise_path))
+        audio_extensions = (".mp3",".wav",".ogg",".flac",".opus",".sphere")
+        files = list(os.listdir(self.noise_path))
+        noise_clips = [f for f in files if f.endswith(audio_extensions)]
         noise_file = self.noise_path/np.random.choice(noise_clips)
         clip_len = self.sample_rate*self.length
-        waveform, _ = torchaudio.load(noise_file, sample_rate=self.sample_rate)
+        waveform, sr = torchaudio.load(noise_file)
+        if sr != self.sample_rate:
+            waveform = torchaudio.functional.resample(
+                    waveform, orig_freq=sr, new_freq=self.sample_rate)
+        waveform = waveform/torch.max(waveform)
         start_idx = np.random.randint(len(waveform)-clip_len)
         return waveform[start_idx, start_idx+clip_len]
