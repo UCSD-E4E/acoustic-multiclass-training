5806ec56034828d22729ca58fff05d00d1549cb0
Benjamin Cates <bjc2dogs@gmail.com>
2023-07-10 17:03:07 -0700

    Make pylint shutup 👿

--- classification/config.py
+++ classification/config.py
@@ -34,6 +34,7 @@ def get_config():
     parser.add_argument('-vf', '--valid_freq', default=1000, type=int)
 
     # Model Training settings
+    # pylint: disable=pointless-string-statement
     """
         Suggested model types:
             eca_nfnet_l0 (90 MB)
