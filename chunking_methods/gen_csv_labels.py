9c6b546dd43530fbd2fa93792eb5fb84a47bc9ce
sprestrelski <samantha.prestrelski@gmail.com>
2023-07-11 18:07:48 -0700

    remove references to file generation

--- chunking_methods/gen_csv_labels.py
+++ chunking_methods/gen_csv_labels.py
@@ -7,8 +7,8 @@ Output:    A csv with chunked, strongly-labeled annotations
 
 import os
 import sys
-import pandas as pd
 from math import ceil
+import pandas as pd
 from pydub import AudioSegment
 from PyHa.IsoAutio import generate_automated_labels
 from WTS_chunking import dynamic_yan_chunking
@@ -20,19 +20,17 @@ ISOLATION_PARAMETERS = {
 }
 
 CHUNK_DURATION = 5
-# produce a wav for each chunk
-GENERATE_WAVS = False
 # incoming filetype
 FILETYPE = ".mp3"
 # use sliding window or raw chunks
 SLIDING_CHUNKS = False
 
 # weak annotations
-METADATA_PATH = "/home/sprestrelski/amabaw1/metadata.csv"
+METADATA_PATH = "~/metadata.csv"
 # output for strong annotations
-STRONG_LABELS_CSV = "/home/sprestrelski/amabaw1/strong_labels.csv"
+STRONG_LABELS_CSV = "~/132PeruXC_Labels.csv"
 # output for chunked annotations
-CHUNKED_CSV = "/home/sprestrelski/amabaw1/raw-chunks.csv"
+CHUNKED_CSV = "~/132PeruXC_RawChunks.csv"
 
 def convert2wav(path):
     """Convert audio files to .wav files with PyDub. Used to ensure
@@ -50,7 +48,8 @@ def convert2wav(path):
             x.export(fn.replace(FILETYPE, '.wav'), format='wav')
 
 def generate_labels(path):
-    """Generate binary time-specific labels using PyHa
+    """Generate binary automated time-specific labels using PyHa
+
     Args:
         path (string)
             - folder path containing audio files with at most one 
@@ -107,53 +106,6 @@ def generate_sliding_chunks():
     unchunked_df = pd.read_csv(STRONG_LABELS_CSV)
     return dynamic_yan_chunking(unchunked_df, chunk_duration=CHUNK_DURATION, only_slide=False)
 
-def generate_wavs_from_labels(path, chunk_duration):
-    """Create wav files based on a .csv with annotations
-    Args:
-        path (string)
-            - folder path containing audio files with at most one 
-            subdirectory level
-        chunk_duration (int)
-            - length of desired file chunks
-    """
-    chunk_path = os.path.join(path, 'chunks')
-    if not os.path.exists(chunk_path):
-        os.makedirs(chunk_path)
-    chunked_df = pd.read_csv(CHUNKED_CSV)
-    file_name = ''
-    label = ''
-    folder_path = ''
-    wav_file = None
-    chunk_count = 0
-    chunk_duration *= 1000
-
-    for _, row in chunked_df.iterrows():
-        # make new folder for each species
-        if row['SPECIES'] != label:
-            label = row['SPECIES']
-            folder_path = os.path.join(chunk_path, label)
-            if not os.path.exists(folder_path):
-                os.makedirs(folder_path)
-        
-        # access the original file
-        if row['FILE NAME'] != file_name:
-            file_name = row['FILE NAME']
-            wav_file_path = os.path.join(path, file_name)
-            wav_file = AudioSegment.from_file(wav_file_path)
-            chunk_count = 1
-
-        # splice wav file and save chunk
-        offset = float(row['OFFSET']) * 1000 # pydub splices in milliseconds, so multiply by 1000
-        chunk = wav_file[offset : offset + chunk_duration]
-
-        try:
-            assert len(chunk) == chunk_duration, f"Chunk of length {chunk_duration / 1000}s could not be \
-                generated from {file_name}. Got chunk of length {len(chunk) / 1000}s. Check chunking script."
-            chunk.export(os.path.join(folder_path, file_name[:-4], '_', str(chunk_count), '.wav'), format='wav')
-        except AssertionError as e:
-            print(e)
-        chunk_count += 1
-
 def generate_raw_chunks(path, chunk_duration):
     """Create .csv annotations for specified second chunks
     Args:
@@ -166,43 +118,59 @@ def generate_raw_chunks(path, chunk_duration):
     chunked_df = pd.DataFrame(columns=["FILE NAME", "FOLDER", "OFFSET", "DURATION", "CLIP LENGTH"])
     chunk_length = chunk_duration * 1000
 
+    metadata_df = pd.read_csv(METADATA_PATH)
+
     files = [f.path for f in os.scandir(path) if f.path.endswith(FILETYPE)]
+    files.sort()
     for f in files:
-        audio = AudioSegment.from_file(f)
+        try:
+            audio = AudioSegment.from_file(f)
+        except RuntimeError as e:
+            print("Audio conversion failed for ", filename + FILETYPE)
+            print(e)
+            continue
         filename = f.split('/')[-1][:-4]
         file_length = len(audio) # in ms
         num_chunks = ceil(file_length / (chunk_length))
-
+        try: 
+            scientific = metadata_df.loc[metadata_df["filename"] == (filename + FILETYPE), 'Scientific Name'].iloc[0]
+            species = metadata_df.loc[metadata_df["filename"] == (filename + FILETYPE), 'Species eBird Code'].iloc[0]
+        except IndexError as e:
+            print("Scientific name or species lookup failed for ", filename + FILETYPE)
+            print(e)
+            continue
         for i in range(num_chunks):
             start = i * chunk_length
             end = start + chunk_length
-
             # cut off ending chunks that won't be 5s long
             if end > file_length:
                 pass
             else:
-                temp_df = {"FILE NAME" : f"{filename}_{i}{FILETYPE}",
-                           "FOLDER" : path,
-                           "OFFSET" : start / 1000,
-                           "DURATION": chunk_duration,
-                           "CLIP LENGTH" : file_length}
-                chunked_df = chunked_df.append(temp_df, ignore_index=True)  
-    return chunked_df
+                temp = [
+                    species,
+                    scientific,
+                    f"{filename}_{i}{FILETYPE}",
+                    path,
+                    start / 1000,
+                    chunk_duration,
+                    file_length
+                ]
+                chunked_df.append(temp)
+    return pd.DataFrame(chunked_df, columns=["SPECIES", "SCIENTIFIC", 
+                                             "FILE NAME", "FOLDER", 
+                                             "OFFSET", "DURATION", "CLIP LENGTH"])
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Incorrect number of args", file=sys.stderr)
-        print("USAGE: python gen_labels_csv.py /path", file=sys.stderr)
+        print("USAGE: python gen_csv_labels.py /path", file=sys.stderr)
         sys.exit(1)
     
-    path = sys.argv[1]
+    filepath = sys.argv[1]
     if SLIDING_CHUNKS:
-        convert2wav(path)
-        generate_labels(path).to_csv(STRONG_LABELS_CSV)
+        convert2wav(filepath)
+        generate_labels(filepath).to_csv(STRONG_LABELS_CSV)
         attach_labels().to_csv(STRONG_LABELS_CSV)
         generate_sliding_chunks().to_csv(CHUNKED_CSV)
     else:
-        generate_raw_chunks(path, CHUNK_DURATION).to_csv(CHUNKED_CSV)
-
-    if GENERATE_WAVS:
-        generate_wavs_from_labels(path, CHUNK_DURATION)
+        generate_raw_chunks(filepath, CHUNK_DURATION).to_csv(CHUNKED_CSV)
