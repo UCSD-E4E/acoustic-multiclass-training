b60a6b460d81c57c96cd5055f5bac8ce30ad7bd2
sprestrelski <samantha.prestrelski@gmail.com>
2023-07-14 19:58:30 -0700

    rename WTS_chunking.py

--- chunking_methods/sliding_chunks.py
+++ chunking_methods/sliding_chunks.py
@@ -11,7 +11,6 @@ def create_chunk_row(row, rows_to_add, new_start, new_end):
             - Row of a single annotation
         rows_to_add (Dataframe)
             - Dataframe of labels
-        
         new_start (float)
             - The start time of the annotation in row
         duration (int)
@@ -25,7 +24,7 @@ def create_chunk_row(row, rows_to_add, new_start, new_end):
     rows_to_add.append(chunk_row.to_frame().T)
     return rows_to_add
 
-def convolving_chunk(row, chunk_duration=3, only_slide=False):
+def convolving_chunk(row, chunk_length_s=3, only_slide=False):
     # TODO: add kwargs to emphasize what things are
     """
     Helper function that converts a Dataframe row containing a binary
@@ -52,43 +51,42 @@ def convolving_chunk(row, chunk_duration=3, only_slide=False):
     rows_to_add = []
     offset = row['OFFSET']
     duration = row['DURATION']
-    chunk_half_duration = chunk_duration / 2
+    chunk_half_duration = chunk_length_s / 2
     
     #Ignore small duration (could be errors, play with this value)
     if duration < 0.4:
         return chunk_df
 
-    if duration <= chunk_duration and not only_slide:
+    if duration <= chunk_length_s and not only_slide:
         #Put the original bird call at...
         #1) Start of clip
-        if (offset+chunk_duration) < row['CLIP LENGTH']:
-            rows_to_add = create_chunk_row(row, rows_to_add, offset, chunk_duration)
+        if (offset+chunk_length_s) < row['CLIP LENGTH']:
+            rows_to_add = create_chunk_row(row, rows_to_add, offset, chunk_length_s)
             
         #2) End of clip
-        if offset+duration-chunk_duration > 0: 
-            rows_to_add = create_chunk_row(row, rows_to_add, offset+duration-chunk_duration, chunk_duration)
+        if offset+duration-chunk_length_s > 0: 
+            rows_to_add = create_chunk_row(row, rows_to_add, offset+duration-chunk_length_s, chunk_length_s)
             
         #3) Middle of clip
         if offset+duration-chunk_half_duration>0 and (offset+duration+chunk_half_duration)< row["CLIP LENGTH"]:
             #Could be better placed in middle, maybe with some randomization?
-            rows_to_add = create_chunk_row(row, rows_to_add, (offset+duration-chunk_half_duration), chunk_duration)
-            
+            rows_to_add = create_chunk_row(row, rows_to_add, (offset+duration-chunk_half_duration), chunk_length_s)
     #Longer than chunk duration
     else:
         #Perform Yan's Sliding Window operation
         clip_num=int(duration/(chunk_half_duration))
         for i in range(clip_num-1):
             new_start = offset+i*chunk_half_duration
-            #new_end = offset + chunk_duration+i*chunk_half_duration
-            if ((offset+chunk_duration)+i*chunk_half_duration) < row['CLIP LENGTH']:
-                rows_to_add = create_chunk_row(row, rows_to_add, new_start, chunk_duration)
+            #new_end = offset + chunk_length_s+i*chunk_half_duration
+            if ((offset+chunk_length_s)+i*chunk_half_duration) < row['CLIP LENGTH']:
+                rows_to_add = create_chunk_row(row, rows_to_add, new_start, chunk_length_s)
     #Add all new rows to our return df
     if len(rows_to_add) == 0:
         return chunk_df
     chunk_df = pd.concat(rows_to_add, ignore_index=True)
     return chunk_df
 
-def dynamic_yan_chunking(df, chunk_duration=3, only_slide=False):
+def dynamic_yan_chunking(df, chunk_length_s=3, only_slide=False):
     """
     Function that converts a Dataframe containing binary
     annotations to uniform chunks of chunk_length. 
@@ -112,6 +110,6 @@ def dynamic_yan_chunking(df, chunk_duration=3, only_slide=False):
     """
     return_dfs = []
     for _, row in df.iterrows():
-        chunk_df = convolving_chunk(row, chunk_duration, only_slide)
+        chunk_df = convolving_chunk(row, chunk_length_s, only_slide)
         return_dfs.append(chunk_df)
     return pd.concat(return_dfs,  ignore_index=True)
