7e8d71f0f595f443ed694a3989e670a7134e1c6b
Marco Bazzani <mbazzani@ucsd.edu>
2023-07-20 10:51:35 -0700

    Updated sweep config

--- classification/sweeps.py
+++ classification/sweeps.py
@@ -5,21 +5,73 @@ cfg = config.cfg
 
 
 sweep_config = {
-    'method'    : 'bayes',
-    'name'      : 'sweep',
-    'metric'    :  {'goal': 'maximize', 'name' : 'val/map'},
-    'parameters': {
-         'batch_size'   : {'values': [16, 32, 64, 128]},
-          'epochs'      : {'values': [5, 10, 15]},
-          'lr'          : {'max': 0.1, 'min': 0.00},
-          'mixup_p'     : {'max': 1., 'min': 0.},
-          'time_shift_p': {'max': 1., 'min': 0.},
-          'noise_p'     : {'max': 1., 'min': 0.},
-          'freq_mask_p' : {'max': 1., 'min': 0.},
-          'time_mask_p' : {'max': 1., 'min': 0.},
-          'rand_eq_p'   : {'max': 1., 'min': 0.}
-          }
-      }
+    'method'            : 'bayes',
+    'name'              : 'sweep',
+    'early_terminate'   : {'type': 'hyperband', 'min_iter': 3},
+    'metric'            : {'goal': 'maximize', 'name' : 'val/map'},
+    'parameters'        : {
+        # General
+        'lr' : {
+            'max': 0.01,
+            'min': 5e-4,
+            'distribution': 'log_uniform_values'
+        },
+        #Data Aug Probabilities
+        'mixup_p'           : {'max': .6, 'min': 0.},
+        'time_shift_p'      : {'max': .6, 'min': 0.},
+        'noise_p'           : {'max': .6, 'min': 0.},
+        'freq_mask_p'       : {'max': .6, 'min': 0.},
+        'time_mask_p'       : {'max': .6, 'min': 0.},
+        'rand_eq_p'         : {'max': .6, 'min': 0.},
+        'bg_noise_p'        : {'max': .6, 'min': 0.},
+        'lowpass_p'         : {'max': .6, 'min': 0.},
+        #Data Aug Params
+        'noise_type': {'values':
+            ['white', 'pink', 'brown', 'blue', 'violet']
+        },
+        'noise_alpha': {'max': .25, 'min': 0.},
+        'freq_mask_param': {'max': 50, 'min': 5},
+        'time_mask_param': {'max': 50, 'min': 5},
+        'mixup_alpha_range': {'values':
+            [[0.1,0.4],
+             [0.0, 0.6],
+             [0.2, 0.3],
+             [0.0, 0.3],
+             [0.3, 0.6]]
+        },
+        'rand_eq_f_range': {'values':
+            [[100, 8000],
+             [20, 8000],
+             [100, 18000],
+             [400, 2000]]
+        },
+        'rand_eq_g_range': {'values':
+            [[-8, 8],
+             [-4, 4],
+             [-2, 8],
+             [-8, 2]]
+        },
+        'rand_eq_q_range': {'values':
+            [[1, 9],
+             [0.3, 9],
+             [0.3, 3]]
+        },
+        'rand_eq_iters': {'max': 5, 'min': 0},
+        'lowpass_cutoff': {
+            'max': 18000,
+            'min': 4000,
+            'distribution': 'log_uniform_values'
+        },
+        'lowpass_q_val': {'max': 3., 'min': 0.1},
+        'bg_noise_alpha_range': {'values':
+            [[0.0, 0.2],
+             [0.0, 0.4],
+             [0.2, 0.3],
+             [0.0, 0.3],
+             [0.0, 0.1]]
+        }
+    }
+}
 
 if __name__ == "__main__":
     wandb.login()
