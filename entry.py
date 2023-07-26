3a190024ac0541eb8ec89a717caf152298c084db
Sean Perry <sean.hyatt.perry@gmail.com>
2023-07-25 22:37:08 -0700

    fix mulitiprocess

--- entry.py
+++ entry.py
@@ -1,7 +1,7 @@
 import pyha_analyzer as pa
 import torch
 
-
-torch.multiprocessing.set_sharing_strategy('file_system')
-torch.multiprocessing.set_start_method('spawn')
-pa.sweeps.main()
+if __name__ == '__main__':
+    torch.multiprocessing.set_sharing_strategy('file_system')
+    torch.multiprocessing.set_start_method('spawn')
+    pa.sweeps.main()
