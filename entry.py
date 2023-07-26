2e42d496bfaec02eac83e38a27a11f2499d88b81
Sean Perry <sean.hyatt.perry@gmail.com>
2023-07-26 09:52:31 -0700

    fix valid issue

--- entry.py
+++ entry.py
@@ -4,4 +4,7 @@ import torch
 if __name__ == '__main__':
     torch.multiprocessing.set_sharing_strategy('file_system')
     torch.multiprocessing.set_start_method('spawn')
-    pa.train.main()
+    if True:
+        pa.train.main()
+    else:
+        pa.sweeps.main()
