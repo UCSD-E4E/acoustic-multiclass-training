5360c905f43b94df7ad64fde5b7c9d5732b38266
Sean Perry <sean.hyatt.perry@gmail.com>
2023-07-26 09:49:20 -0700

    swap to train

--- entry.py
+++ entry.py
@@ -4,4 +4,4 @@ import torch
 if __name__ == '__main__':
     torch.multiprocessing.set_sharing_strategy('file_system')
     torch.multiprocessing.set_start_method('spawn')
-    pa.sweeps.main()
+    pa.train.main()
--- pyha_analyzer/__init__.py
+++ pyha_analyzer/__init__.py
@@ -1,5 +1,6 @@
 """ Gets project directory for config """
 from os import path
 from . import sweeps  
+from . import train
 
 pyha_project_directory = path.dirname(path.dirname(__file__))
