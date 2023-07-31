e0c307af3aa3ed86b693c29af67486071efe5867
Marco Bazzani <mbazzani@ucsd.edu>
2023-07-31 10:23:58 -0700

    Reworked sweeps to use yml config

--- pyha_analyzer/sweeps.py
+++ pyha_analyzer/sweeps.py
@@ -6,89 +6,12 @@ sweep. If the former, set the sweep_id option in config
 
 import torch
 import wandb
+import yaml
 
 from pyha_analyzer import config
 from pyha_analyzer.train import main as train_main
 
 cfg = config.cfg
-
-
-sweep_config = {
-    'method'            : 'bayes',
-    'name'              : 'sweep',
-    'early_terminate'   : {'type': 'hyperband', 'min_iter': 3},
-    'metric'            : {'goal': 'maximize', 'name' : 'valid/map'},
-    'parameters'        : {
-        # General
-        'learning_rate' : {
-            'max': 0.01,
-            'min': 5e-4,
-            'distribution': 'log_uniform_values'
-        },
-        'epochs' : {'values':[3]},
-        'model' : {'values':["eca_nfnet_l0",
-                "eca_nfnet_l0",
-                 "tf_efficientnet_b4",
-                 "tf_efficientnet_b4",
-                 "resnetv2_101",
-                 "seresnext101_32x4d",
-                 "rexnet_200"]},
-        #Data Aug Probabilities
-        'mixup_p'           : {'max': .6, 'min': 0.},
-        'time_shift_p'      : {'max': .6, 'min': 0.},
-        'noise_p'           : {'max': .6, 'min': 0.},
-        'freq_mask_p'       : {'max': .6, 'min': 0.},
-        'time_mask_p'       : {'max': .6, 'min': 0.},
-        'rand_eq_p'         : {'max': .6, 'min': 0.},
-        'bg_noise_p'        : {'max': .6, 'min': 0.},
-        'lowpass_p'         : {'max': .6, 'min': 0.},
-        #Data Aug Params
-        'noise_type': {'values':
-            ['white', 'pink', 'brown', 'blue', 'violet']
-        },
-        'noise_alpha': {'max': .25, 'min': 0.},
-        'freq_mask_param': {'max': 50, 'min': 5},
-        'time_mask_param': {'max': 50, 'min': 5},
-        'mixup_alpha_range': {'values':
-            [[0.1,0.4],
-             [0.0, 0.6],
-             [0.2, 0.3],
-             [0.0, 0.3],
-             [0.3, 0.6]]
-        },
-        'rand_eq_f_range': {'values':
-            [[100, 8000],
-             [20, 8000],
-             [100, 18000],
-             [400, 2000]]
-        },
-        'rand_eq_g_range': {'values':
-            [[-8, 8],
-             [-4, 4],
-             [-2, 8],
-             [-8, 2]]
-        },
-        'rand_eq_q_range': {'values':
-            [[1, 9],
-             [0.3, 9],
-             [0.3, 3]]
-        },
-        'rand_eq_iters': {'max': 5, 'min': 0},
-        'lowpass_cutoff': {
-            'max': 18000,
-            'min': 4000,
-            'distribution': 'log_uniform_values'
-        },
-        'lowpass_q_val': {'max': 3., 'min': 0.1},
-        'bg_noise_alpha_range': {'values':
-            [[0.0, 0.2],
-             [0.0, 0.4],
-             [0.2, 0.3],
-             [0.0, 0.3],
-             [0.0, 0.1]]
-        }
-    }
-}
 
 def main():
     """
@@ -99,6 +22,8 @@ def main():
     sweep_project = f"{cfg.wandb_project}-sweep"
     if not sweep_id:
         print("Starting a new sweep")
+        f = open("sweep.yml", 'r')
+        sweep_config = yaml.safe_load(f)
         sweep_id = wandb.sweep(
             sweep_config,
             entity=cfg.wandb_entity,
