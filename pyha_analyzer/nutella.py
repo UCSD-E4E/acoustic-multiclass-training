03de22e3b3ef59e3fdf0d1468454665a65998f97
Marco Bazzani <mbazzani@ucsd.edu>
2023-08-18 15:26:51 -0700

    Set multiprocessing method to spawn

--- pyha_analyzer/nutella.py
+++ pyha_analyzer/nutella.py
@@ -337,4 +337,6 @@ def main():
     finetune(model)
 
 if __name__=="__main__":
+    torch.multiprocessing.set_sharing_strategy('file_system')
+    torch.multiprocessing.set_start_method('spawn')
     main()
